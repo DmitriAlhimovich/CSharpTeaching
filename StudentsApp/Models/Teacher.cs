8d62a8b3d278cbc2897964782841b2dc18c7c73b
alhimovich <alhimovich@ALHIMOVICH>
2021-03-15 10:30:09 +0300

    Teacher formatting

--- StudentsApp/Models/Teacher.cs
+++ StudentsApp/Models/Teacher.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace StudentsApp.Models
 {
-    public class Teacher:Person
+    public class Teacher : Person
     {
         public List<Subject> Subjects { get; set; }
     }
