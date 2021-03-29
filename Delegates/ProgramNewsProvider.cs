694fa5b88fa1628b07685eb6975c7e1d2a026df3
alhimovich <alhimovich@ALHIMOVICH>
2021-03-29 16:23:53 +0300

    Delegates

---
+++ Delegates/ProgramNewsProvider.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharp
+{
+    //задание - создать делегат для события EconomicNewsCreated с 2мя параметрами - текст и время, подписаться на это событие
+
+    class NewsProvider
+    {
+        public delegate void CreateNews();
+
+        public delegate void CreateNewsWithText(string text);
+
+        public event CreateNews SportNewCreated;
+        public event CreateNewsWithText MusicNewsCreated;
+        public event CreateNews EconomicNewsCreated;
+
+        public void OnSportNewsCreated()
+        {
+            SportNewCreated();
+        }
+
+        public void OnMusicNewsCreated()
+        {
+            MusicNewsCreated(DateTime.Now.ToShortTimeString());
+        }
+
+    }
+
+    class NewsSubscriber
+    {
+        private string name;
+        public NewsSubscriber(string name)
+        {
+            this.name = name;
+        }
+
+        public void SportNewHandler()
+        {
+            Console.WriteLine("Sport news was received");
+            Console.WriteLine($"Subscriber = {name}");
+        }
+
+        public void MusicNewsHandler(string text)
+        {
+            Console.WriteLine($"music news - {text}, Subscriber = {name}");
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NewsProvider tass = new NewsProvider();
+
+            NewsSubscriber tutby = new NewsSubscriber("tut");
+            NewsSubscriber ont = new NewsSubscriber("ont");
+            NewsSubscriber bt = new NewsSubscriber("bt");
+
+
+            tass.SportNewCreated += tutby.SportNewHandler;
+
+            tass.MusicNewsCreated += ont.MusicNewsHandler;
+            tass.MusicNewsCreated += bt.MusicNewsHandler;
+
+            tass.OnSportNewsCreated();
+            tass.OnMusicNewsCreated();
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Delegates/ProgramSimple.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace Delegates
+{
+    class ProgramSimple
+    {
+		delegate double Average(double[] numbers);
+
+		delegate int Operation(int x, int y);
+		public static void Main()
+		{
+
+			Average avg;
+
+
+			var numbers = new double[] { 3, 4, 5, 6 };
+
+			avg = GetAverageSimple;
+
+			var resultSimple = avg(numbers);
+
+			avg = GetAverageLinq;
+			var resultLinq = avg(numbers);
+
+
+			Operation operation = (x, y) => x + y;
+			Console.WriteLine(operation(10, 20));       // 30
+		}
+
+		private static double GetAverageSimple(double[] numbers)
+        {
+			return numbers.Sum() / numbers.Count();
+        }
+
+		private static double GetAverageLinq(double[] numbers)
+		{
+			return numbers.Average();
+		}
+
+	}
+}
