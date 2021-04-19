6cd77efd09012bd9c2f3b0a4fbbfa6945f70e0b8
alhimovich <alhimovich@ALHIMOVICH>
2021-04-19 17:08:24 +0300

    Multithreading, semaphore

---
+++ MultiThreading/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading;
+
+namespace MultiThreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sizeThread = 20;
+            int sizeMain = 20;
+
+            //int[] inputs = new int[] { 999999999, 888888888, 998990996, 667889776, 955755655, 994995996, 911822733 };
+            long[] inputs = new long[] { 2101000000, 1800000000, 1501000000, 2110000000 };
+
+            var startTime = DateTime.Now;
+            Console.WriteLine($"Using single thread. Started: {startTime.ToLongTimeString()}");
+
+            foreach (var input in inputs)
+                Sum(input);
+
+            var finishTime = DateTime.Now;
+            Console.WriteLine($" Single thread - Finished at {finishTime.ToLongTimeString()}. Total time consumed (in sec): {(finishTime - startTime).TotalSeconds}");
+
+            startTime = DateTime.Now;
+            Console.WriteLine($"Using multi threads. Started: {startTime.ToLongTimeString()}");
+
+
+            foreach (var input in inputs)
+            {
+                // создаем новый поток
+                Thread myThread = new Thread(() => Sum(input));
+                myThread.Start(); // запускаем поток
+            }
+
+            Console.WriteLine($" Multi threads - Total time consumed (in sec): {(DateTime.Now - startTime).TotalSeconds}");
+
+            Console.ReadLine();
+        }
+
+        public static void Sum(long input)
+        {
+            Console.WriteLine($"-------  input {input}. Started: {DateTime.Now.ToLongTimeString()}");
+            var startTime = DateTime.Now;
+            long result = 1;
+            for (int i = 1; i <= input; i++)
+                result += i;
+
+            Console.WriteLine($"--------  Sum of {input} equals {result: # ###}, finished at {DateTime.Now.ToLongTimeString()}. Time consumed (in sec): {(DateTime.Now - startTime).TotalSeconds}");
+        }
+
+
+        public static void Count(int size)
+        {
+            for (int i = 1; i < size; i++)
+            {
+                Console.WriteLine("Второй поток:");
+                Console.WriteLine(i * i);
+                Thread.Sleep(400);
+            }
+        }
+    }
+}
---
+++ MultiThreading/Semaphore.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace MultiThreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 5; i++)
+            {
+                Reader reader = new Reader(i);
+            }
+
+            Console.WriteLine("Finished");            
+        }
+    }
+
+    class Reader
+    {
+        // создаем семафор
+        static Semaphore sem = new Semaphore(5, 5);
+        Thread myThread;
+        int count = 3;// счетчик чтения
+
+        public Reader(int i)
+        {
+            myThread = new Thread(Read);
+            myThread.Name = $"Читатель {i}";
+            myThread.Start();
+        }
+
+        public void Read()
+        {
+            while (count > 0)
+            {
+                sem.WaitOne();
+
+                Console.WriteLine($"{Thread.CurrentThread.Name} входит в библиотеку");
+
+                Console.WriteLine($"{Thread.CurrentThread.Name} читает");
+                Thread.Sleep(1000);
+
+                Console.WriteLine($"{Thread.CurrentThread.Name} покидает библиотеку");
+
+                sem.Release();
+
+                count--;
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
